b0c4e52c954719c9a758281de5ec137a02a0b04c
Kris Poedjosoedarmo <neorou@gmail.com>
2017-04-23 16:09:40 -0400

    revised testing pg connections

--- user.js
+++ user.js
@@ -1,29 +1,37 @@
 // database utility that accesses user and user related mechanisms
 
-const pg = require('pg');
+var getUsers = function(dbQuery, dbqArgs, cb) {
+		
+		const pg = require('pg');
+		var config = {
+				user: 'fillitup',
+				database: 'fillitup',
+				password: 'fillitup',
+				host: 'localhost',
+				port: 5432
+		};
 
-var config = {
-    user: 'fillitup',
-    database: 'fillitup',
-    password: 'fillitup',
-    host: 'localhost',
-    port: 5432
+		var client = new pg.Client();
+		client.connect(function (err) {
+				if (err) throw err;
+
+				// one query just to show it works
+				client.query(dbQuery, dbqArgs, function (err, result) {
+						if (err) throw err;
+						// console.log(result.rows[0]);
+						client.end(function (err) {
+								if (err) throw err;
+								cb(null, result);
+						});
+				});
+		});
 };
 
-var client = new pg.Client();
+console.log('test');
 
-client.connect(function (err) {
-    if (err) throw err;
-
-    // one query just to show it works
-    client.query('SELECT * from t_user', [], function (err, result) {
-        if (err) throw err;
-        console.log(result.rows[0]);
-        client.end(function (err) {
-            if (err) throw err;
-        });
-    });
+var dbQuery = 'select * from t_user';
+var dbqArgs = {};
+getUsers(dbQuery, dbqArgs, function(err, results) {
+		if (err) { console.log(err); }
+		console.log(null, results);
 });
-
-
-
