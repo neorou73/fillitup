921ff1229b7b1be0658807e8b305971d8e43128f
Kris Poedjosoedarmo <neorou@gmail.com>
2017-04-23 18:28:29 -0400

    added new sql statements for users

--- user.js
+++ user.js
@@ -22,6 +22,27 @@ var getUsers = function(dbQuery, dbqArgs, cb) {
 		});
 };
 
+/*user:
+    * create user
+    * delete user
+    * update user
+    * read user
+    * update password
+    * update email
+    * activate user
+    * deactivate user
+    * add user to group
+    * remove user from group
+
+*/
+
+var queryStatement = {
+		'createUser': 'insert into t_user (name, username, password, email, created, status) values ($1, $2, $3, $4, $5, $6)',
+		'deleteUser': 'delete from t_user where id = $1',
+		'updateUser': 'update t_user set name = $1, password = $2, email = $3, status = $4 where id = $5',
+		'readUser': 'select * from t_user where email = $1'
+};
+
 console.log('test');
 
 var dbQuery = 'select NOW()';
