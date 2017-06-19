221cfb71505207976c5090844976dad397d6e12a
Kris Poedjosoedarmmo <neorou@gmail.com>
2017-06-19 13:24:56 +0000

    switching over to using flask

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from bottle import route, run, template
+# all the imports
 import json
+from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 
 def readConfig():
     sourceFile = 'config.default.json'
@@ -10,8 +11,10 @@ def readConfig():
 
 configuration = readConfig()
 
-@route('/hello/<name>')
-def index(name):
-    return template('<b>Hello {{name}}</b>!', name=name)
+# run(host=configuration['fillitup']['application-host'], port=configuration['fillitup']['application-port'], debug=True)
 
-run(host=configuration['fillitup']['application-host'], port=configuration['fillitup']['application-port'], debug=True)
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
