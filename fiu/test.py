967d5bce86b5701d5233d087cdbc0ebe97167435
Kris Poedjosoedarmo <neorou@gmail.com>
2017-10-15 22:08:10 -0400

    update

--- fiu/test.py
+++ fiu/test.py
@@ -5,16 +5,45 @@ import unittest
 
 class TestCrud(unittest.TestCase):
 
-    def test_canCreateAndDestroyUser(self):
-        from fillupUser import fillupUser
-        fuu = fillupUser()
+    def setUp(self):
         testUser = {}
         testUser["fullname"] = "Test User"
         testUser["username"] = "testuser1"
         testUser["email"] = "neorou@yahoo.com"
         testUser["password"] = "5ecret!"
+        self.testUser = testUser
 
-        success = fuu.createUser(testUser)
+        updatedUserData = {}
+        updatedUserData["fullname"] = "Test User Updated"
+        updatedUserData["username"] = "testuser2"
+        updatedUserData["password"] = "5ecret!2"
+        updatedUserData["email"] = "neorou@yahoo.com"
+        self.updatedUserData = updatedUserData
+        
+
+    def test_canConnectToDatabase(self):
+        from fillupDbc import psqlConnect
+        pc = psqlConnect()
+        pc.connect()
+        pc.cursor.execute("""SELECT now();""")
+        rows = pc.cursor.fetchall()
+        self.assertTrue(len(rows) > 0)
+
+
+    def test_canQueryDatabase(self):
+        from fillupDbc import psqlConnect
+        pc = psqlConnect()
+        pc.connect()
+        pc.cursor.execute("""SELECT now();""")
+        rows = pc.cursor.fetchall()
+        self.assertTrue(len(rows) > 0)
+
+
+    def test_canCreateAndDestroyUser(self):
+        from fillupUser import fillupUser
+        fuu = fillupUser()
+
+        success = fuu.createUser(self.testUser)
         self.assertTrue(success)
         success2 = fuu.deleteUser("neorou@yahoo.com")
         self.assertEqual(success2, "success")
@@ -23,20 +52,9 @@ class TestCrud(unittest.TestCase):
     def test_canEditUser(self):
         from fillupUser import fillupUser
         fuu = fillupUser()
-        testUser = {}
-        testUser["fullname"] = "Test User"
-        testUser["username"] = "testuser1"
-        testUser["email"] = "neorou@yahoo.com"
-        testUser["password"] = "5ecret!"
+        fuu.createUser(self.testUser)
 
-        fuu.createUser(testUser)
-        updatedUserData = {}
-        updatedUserData["fullname"] = "Test User Updated"
-        updatedUserData["username"] = "testuser2"
-        updatedUserData["password"] = "5ecret!2"
-        updatedUserData["email"] = "neorou@yahoo.com"
-
-        success = fuu.updateUser("neorou@yahoo.com", updatedUserData)
+        success = fuu.updateUser("neorou@yahoo.com", self.updatedUserData)
         self.assertTrue(success)
         fuu.deleteUser("neorou@yahoo.com")
 
