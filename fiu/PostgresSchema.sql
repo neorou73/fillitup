b3ebdc411626747b2371065111d6874e1ab0b174
Kris Poedjosoedarmo <neorou@gmail.com>
2017-10-22 20:22:43 +0000

    added drop and create database

--- fiu/PostgresSchema.sql
+++ fiu/PostgresSchema.sql
@@ -3,18 +3,35 @@ connection to postgresql database
 create user and tables
 */
 
-create table appuser (
-  id serial, fullname varchar(1024), username varchar(1024), 
-  email varchar(1024), password varchar(1024), created date default current_timestamp, 
-  groups text, active boolean default true, primary key(id));
+drop table logintoken cascade;
+/*drop sequence logintoken_id_seq cascade;*/
+drop table appgroup cascade;
+/*drop sequence appgroup_id_seq cascade;*/
+drop table appuser cascade;
+/*drop sequence appuser_id_seq cascade;*/
+drop table appdocument cascade;
+/*drop sequence appdocument_id_seq cascade;*/
 
+drop database if exists fillup;
+create database fillup;
+
+drop user if exists fillup;
+create user fillup with password 'fillup' valid until 'infinity';
+alter database fillup owner to fillup;
+
+
+
+create table appuser (
+  id serial, fullname varchar(1024), username varchar(1024),
+  email varchar(1024), password varchar(1024), created date default current_timestamp,
+  groups text, active boolean default true, primary key(id));
 alter table appuser owner to fillup;
+
 
 create table appgroup (
   id serial, groupname varchar(1024), created date default current_timestamp,
   description text,
   active boolean default true, primary key (id));
-
 alter table appgroup owner to fillup;
 
 insert into appuser (fullname, username, email, password, created, groups) values
@@ -28,14 +45,11 @@ create table logintoken (
   id serial,
   token text not null, appuser integer not null references appuser (id),
   created date not null default current_timestamp, logged_out date, primary key (id));
-
 alter table logintoken owner to fillup;
 
 create table appDocument (
-  id varchar(50), title text, 
+  id varchar(50), title text,
   creator integer not null references appuser (id),
   document json not null,
   created date default current_timestamp, primary key (id));
-
 alter table appDocument owner to fillup;
-
