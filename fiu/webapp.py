c9ddc4b603515432b59d118c20b6fe5438333e87
Kris Poedjosoedarmo <neorou@gmail.com>
2017-11-17 15:42:40 -0500

    Sample document post processing

--- fiu/webapp.py
+++ fiu/webapp.py
@@ -1,12 +1,27 @@
-from bottle import route, run, template
+from bottle import route, run, template, get, post, request
 
 @route('/')
 def index():
-    return "<h1>Fill It Up</h1>"
+    return "<h1>Fill It Up</h1><p><a href='/new'>record a new document</a></p>"
 
 @route('/process/<data>')
 def process(data):
     return template('you provided: {{data}}', data=data)
+
+@route('/new')
+def newDocumentForm():
+    return """
+        <form action='new' method='post'>
+            <label>content:</label>
+            <textarea rows=5 cols=15 name='document'></textarea>
+            <input value='submit' type='submit'/>
+        </form>
+    """
+
+@route('/new', method='post')
+def newDocumentPost():
+    documentData = request.forms.get('document')
+    return documentData
 
 # run(host='localhost', port=8080)
 run(host='0.0.0.0', port=8080, server='gunicorn', workers=4, debug=True)
