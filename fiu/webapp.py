8f5ed5a207b3ae23853459c579a34c54bd0da384
Kris Poedjosoedarmo <neorou@gmail.com>
2017-11-17 12:46:16 -0500

    expanded webapp and added gunicorn sample with testing

--- fiu/webapp.py
+++ fiu/webapp.py
@@ -1,8 +1,12 @@
-from bottle import route, run
+from bottle import route, run, template
 
 @route('/')
 def index():
     return "<h1>Fill It Up</h1>"
 
+@route('/process/<data>')
+def process(data):
+    return template('you provided: {{data}}', data=data)
+
 # run(host='localhost', port=8080)
-run(host='localhost', port=8080, debug=True)
+run(host='0.0.0.0', port=8080, server='gunicorn', workers=4, debug=True)
