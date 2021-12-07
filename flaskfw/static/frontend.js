6fac6864f23e2ae57f7adf8502633e2552ec9546
Kris Poedjosoedarmo <neorou@gmail.com>
2021-12-07 12:41:01 -0500

    login using localStorage to manage state

--- flaskfw/static/frontend.js
+++ flaskfw/static/frontend.js
@@ -2,7 +2,41 @@ $( document ).ready(function() {
     console.log( "document frontend.js ready!" )
     const location = window.location
     console.log(location) // use pathname 
-    
+    const loggedIn = localStorage.getItem('fiuscope_at')
+    const showLogin = (status) => {
+        if (status) {
+            $("#loginFormDiv").show()
+            $("#logoutDiv").hide()
+        } else {
+            $("#loginFormDiv").hide()
+            $("#logoutDiv").show()
+        }
+    }
+
+    if (localStorage.getItem('fiuscope_at') != null) {
+        console.log('you are logged you in')
+        console.log("access token value is " + localStorage.getItem('fiuscope_at'))
+        console.log("scope count is " + localStorage.getItem('fiuscope_count'))
+        showLogin(false)
+    } else {
+        console.log('logging you in')
+        localStorage.setItem('fiuscope_count', 1)
+        localStorage.setItem('fiuscope_at', 'asdf1234')
+        showLogin(true)
+    }
+
+    const ct = parseInt(localStorage.getItem('fiuscope_count'))
+    if (ct >= 3) {
+        console.log('logging you out')
+        localStorage.removeItem('fiuscope_at')
+        localStorage.removeItem('fiuscope_count')
+        localStorage.clear()
+        showLogin(false)
+    } else {
+        localStorage.setItem('fiuscope_count', (ct + 1))
+        showLogin(true)
+    }
+
     if (location.pathname.substring(0, 8) == "/editor") {
         $.get("/static/fe_editor.html", function(data){
             $("#includedHtml").html(data);
