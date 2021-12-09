120ccbac8ca857e03ca9cf5b40923b69f8528c23
Kris Poedjosoedarmo <neorou@gmail.com>
2021-12-09 09:35:58 -0500

    meta tag keyword selector

--- flaskfw/static/fe_manage.js
+++ flaskfw/static/fe_manage.js
@@ -1,4 +1,16 @@
 $( document ).ready(function() {
     console.log( "document fe_manage.js ready!" )
-    
+    const showOneMngDiv = (visibleDiv) => {
+        $(".management-div").hide()
+        $(visibleDiv).show()
+    }
+    showOneMngDiv("#mng-users") // default div to show 
+    $(".select-mng-div").click((e) => {
+        console.log(e.target)
+        $(".select-mng-div").removeClass('keywordTagged')
+        $(('#' + e.target.id)).addClass('keywordTagged')
+        const eTargetId = "#mng-" + (e.target.id).replace("select-","").replace("-mng","")
+        console.log(eTargetId)
+        showOneMngDiv(eTargetId)
+    })
 })
